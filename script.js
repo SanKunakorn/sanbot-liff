d65e3d81431012a1459882df5b481c2fc9d0ca11
SanKunakorn <89127130+SanKunakorn@users.noreply.github.com>
2024-04-15 15:31:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+getLocation()
+    let lat, lon
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                lat = position.coords.latitude
+                lon = position.coords.longitude
+                //แสดง lat long ใน textbox
+                document.getElementById("latlong").value=lat+','+lon;
+            });
+        }
+    }
